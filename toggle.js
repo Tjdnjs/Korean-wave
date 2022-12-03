5392d213543108af4d1e50f8c6acf5e6d00cd9ce
Tjdnjs <2seowon@naver.com>
2022-12-03 15:54:54 +0900

    [update] toggle.js struct

--- toggle.js
+++ toggle.js
@@ -10,10 +10,37 @@ function selectOption(optionElement) {
   selectedElement.textContent = optionElement.textContent;
 }
 
+function backHover(target, color1, color2){
+  target.forEach(el =>{
+    el.addEventListener("mouseover", e => {
+      el.style.background = color1;
+    });
+    el.addEventListener("mouseout", e => {
+      el.style.background = color2;
+    })
+  })
+}
+
+function colorHover(target, color1, color2){
+  target.forEach(el =>{
+    el.addEventListener("mouseover", e => {
+      el.style.color = color1;
+    });
+    el.addEventListener("mouseout", e => {
+      el.style.color = color2;
+    })
+  })
+}
+
 selectBoxElements.forEach(selectBoxElement => {
-  selectBoxElement.addEventListener("click", function (e) {
+  selectBoxElement.addEventListener("click", e => {
     const targetElement = e.target;
     const isOptionElement = targetElement.classList.contains("option");
+
+    if (isOptionElement) {
+      selectOption(targetElement);
+    }
+
     const theme = targetElement.innerHTML;
     const body = document.querySelector("body");
     const svg = document.querySelector(".custom-shape-divider-top-1668397205 .shape-fill")
@@ -21,101 +48,65 @@ selectBoxElements.forEach(selectBoxElement => {
     const nav = document.querySelectorAll("nav a");
     const text1 = Array.prototype.slice.call(document.querySelectorAll(".theme, .option"));
     const options = document.querySelectorAll('.option');
-    console.log(text);
 
-    if (isOptionElement) {
-      selectOption(targetElement);
-    }
+    //? 테마가 Plain으로 설정 되었을 때
     if (theme == "Plain") {
       body.style.background = '#EBEBEB';
       svg.style.fill = 'black';
-      text.forEach(function(e){
-        e.style.color = 'black';
-      });
-      text1.forEach(function(e){
-        e.style.color = '#818181';
-      })
-      nav.forEach(n =>{
-        n.addEventListener("mouseover", function (e){
-          n.style.color = 'gray';
-        });
-        n.addEventListener("mouseout", function (e){
-          n.style.color = 'black';
-        });
-      })
+      text.forEach(e => {e.style.color = 'black';});
+      text1.forEach(e => {e.style.color = '#818181';})
+
+      // option과 nav 대한 hover 처리
+      backHover(options,'rgb(255, 255, 255)','rgba(255, 255, 255, .4)');
+      colorHover(nav, 'gray', 'black');
+
       document.querySelector(".theme").style.border='1px solid #7a7a7a';
     }
 
+    //? 테마가 Dark로 설정 되었을 때
     else if (theme == "Dark") {
         text = text.concat(text1); 
         console.log("text: " + text);
         body.style.background = 'black';
-        text.forEach(function(e){
-          e.style.color = 'white';
-        });
-        options.forEach(option =>{
-          option.addEventListener("mouseover", function(e){
-            option.style.background = 'gray';
-          });
-          option.addEventListener("mouseout", function(e){
-            option.style.background = 'rgba(255, 255, 255, .4)';
-          })
-        })
-        nav.forEach(n =>{
-          n.addEventListener("mouseover", function (e){
-            n.style.color = 'rgba(255, 255, 255, .8)';
-          });
-          n.addEventListener("mouseout", function (e){
-            n.style.color = 'rgb(255, 255, 255)';
-          });
-        })
+        text.forEach(e => {e.style.color = 'white';});
+
+        // option과 nav 대한 hover 처리
+        backHover(options, 'gray', 'rgba(255, 255, 255, .4)');
+        colorHover(nav, 'rgba(255, 255, 255, .8)', 'rgb(255, 255, 255)');
+        
         svg.style.fill = '#FDFDFD';
     }
 
+    //? 테마가 Korea로 설정 되었을 때
     else if (theme == "Korea") {
         console.log("korea");
         body.style.background = 'linear-gradient(142deg, #FFB5B5, #9CA0FF)';
         svg.style.fill = '#FDFDFD';
-        text.forEach(function(e){
-          e.style.color = 'rgba(255, 255, 255,.8)';
-        });
-        text1.forEach(function(e){
-          e.style.color = '#818181';
-        })
-        options.forEach(option =>{
-          option.addEventListener("mouseover", function(e){
-            option.style.background = 'rgb(255, 255, 255)';
-          });
-          option.addEventListener("mouseout", function(e){
-            option.style.background = 'rgba(255, 255, 255, .4)';
-          })
-        })
-        nav.forEach(n =>{
-          n.addEventListener("mouseover", function (e){
-            n.style.color = 'rgb(255, 255, 255)';
-          });
-          n.addEventListener("mouseout", function (e){
-            n.style.color = 'rgba(255, 255, 255,.8)';
-          });
-        })
+        text.forEach(e => {e.style.color = 'rgba(255, 255, 255,.8)';});
+        text1.forEach(e => {e.style.color = '#818181';})
+
+        // option과 nav 대한 hover 처리
+        backHover(options,'rgb(255, 255, 255)','rgba(255, 255, 255, .4)');
+        colorHover(nav, 'rgb(255, 255, 255)', 'rgba(255, 255, 255, .8)');
+
         document.querySelector(".theme").style.border='none';
     }
-
+    //? 테마가 설정되지 않았을 때 (처음 화면이 켜진 상태)
     else {
       body.style.background = 'linear-gradient(142deg, rgba(255,181,181,1) 0%, rgba(156,160,255,1) 88%);';
       svg.style.fill = '#FDFDFD';
     }
+    
     toggleSelectBox(selectBoxElement);
   });
 });
 
-document.addEventListener("click", function (e) {
+//* 빈 배경을 클릭했을 때 드롭다운 되어있던 select 메뉴들이 접히게 함 
+document.addEventListener("click", e => {
   const targetElement = e.target;
   const isSelect = targetElement.classList.contains("select") || targetElement.closest(".select");
 
-  if (isSelect) {
-    return;
-  }
+  if (isSelect) return;
 
   const allSelectBoxElements = document.querySelectorAll(".select");
 
